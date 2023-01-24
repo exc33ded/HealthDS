03ad8611bd0d5df10c98f4b34189ca9c5f2c580b
Sarim <sarimisraiely12@gmail.com>
2023-01-24 23:52:40 +0530

    commit

--- website/views.py
+++ website/views.py
@@ -135,3 +135,70 @@ def heart():
 @login_required
 def heart_history():
     return render_template('heart_history.html', user=current_user)
+
+@views.route("/bcancer", methods=['GET', 'POST'])
+@login_required
+def bcancer():
+    if request.method == 'GET':
+        return render_template('cancer_pred.html')
+    else: 
+        radius_mean = float(request.form['radius_mean'])
+        texture_mean = float(request.form['texture_mean']) 
+        perimeter_mean = float(request.form['perimeter_mean'])
+        area_mean = float(request.form['area_mean']) 
+        smoothness_mean = float(request.form['smoothness_mean']) 
+        compactness_mean = float(request.form['compactness_mean']) 
+        concavity_mean = float(request.form['concavity_mean'])
+        concave_points_mean = float(request.form['concave_points_mean']) 
+        symmetry_mean = float(request.form['symmetry_mean']) 
+        fractal_dimension_mean = float(request.form['fractal_dimension_mean'])
+        radius_se = float(request.form['radius_se']) 
+        texture_se = float(request.form['texture_se']) 
+        perimeter_se = float(request.form['perimeter_se']) 
+        area_se = float(request.form['area_se']) 
+        smoothness_se = float(request.form['smoothness_se'])
+        compactness_se = float(request.form['compactness_se']) 
+        concavity_se = float(request.form['concavity_se']) 
+        concave_points_se = float(request.form['concave_points_se']) 
+        symmetry_se = float(request.form['symmetry_se'])
+        fractal_dimension_se = float(request.form['fractal_dimension_se']) 
+        radius_worst = float(request.form['radius_worst']) 
+        texture_worst = float(request.form['texture_worst'])
+        perimeter_worst = float(request.form['perimeter_worst']) 
+        area_worst = float(request.form['area_worst']) 
+        smoothness_worst = float(request.form['smoothness_worst'])
+        compactness_worst = float(request.form['compactness_worst']) 
+        concavity_worst = float(request.form['concavity_worst']) 
+        concave_points_worst = float(request.form['concave_points_worst'])
+        symmetry_worst = float(request.form['symmetry_worst']) 
+        fractal_dimension_worst = float(request.form['fractal_dimension_worst'])
+
+        model_bcancer = pickle.load(open('model/cancer_final_model.pkl', 'rb'))
+        input_data = (radius_mean, texture_mean, perimeter_mean,
+                    area_mean, smoothness_mean, compactness_mean, concavity_mean,
+                    concave_points_mean, symmetry_mean, fractal_dimension_mean,
+                    radius_se, texture_se, perimeter_se, area_se, smoothness_se,
+                    compactness_se, concavity_se, concave_points_se, symmetry_se,
+                    fractal_dimension_se, radius_worst, texture_worst,
+                    perimeter_worst, area_worst, smoothness_worst,
+                    compactness_worst, concavity_worst, concave_points_worst,
+                    symmetry_worst, fractal_dimension_worst)
+
+        print(input_data)
+
+        input_data_as_numpy_array= np.asarray(input_data)
+        input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+        print(input_data_reshaped)
+
+        
+
+        prediction = model_bcancer.predict(input_data_reshaped)
+        senddata=""
+        if (prediction[0] == 0):
+            predi = "Benign"
+            senddata='According to the given details the chance of Benign is '
+        else:
+            predi = "Malignant"
+            senddata='According to the given details the chance of Malignant is '
+
+        return render_template('cancer_pred.html', prediction_text=senddata)
